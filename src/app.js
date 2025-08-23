51f1e6ef15d44a72f38d379cd1b627008c34b6f4
kert <franciskertdn@gmail.com>
2025-08-23 20:01:36 +0800

    done contact

--- src/app.js
+++ src/app.js
@@ -9,15 +9,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts);
-app.set('layout', 'layouts/main'); // ✅ singular "layout"
+app.set('layout', 'layouts/main'); 
 
 // Routes
 const mainroutes = require('./routes/index');
 const aboutroutes = require('./routes/about');
 const project = require('./routes/project');
+const contactroutes = require('./routes/contact');
 app.use('/', mainroutes);
 app.use('/', aboutroutes);
 app.use('/', project);
+app.use('/', contactroutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
